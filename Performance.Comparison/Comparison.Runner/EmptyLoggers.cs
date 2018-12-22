df7a5d8d1feac861d1f85e71d970c48b112996df
Igor Manushin <igor.manushin@gmail.com>
2018-12-22 11:10:51 +0300

    add small test

---
+++ Performance.Comparison/Comparison.Runner/EmptyLoggers.cs
@@ -0,0 +1,31 @@
+﻿using BenchmarkDotNet.Attributes;
+using log4net;
+using NLog;
+using LogManager = log4net.LogManager;
+
+namespace Comparison.Runner
+{
+    [ClrJob(baseline: true)]
+    [RPlotExporter, RankColumn]
+    internal sealed class EmptyLoggers
+    {
+        private ILog _log4Net;
+        private Logger _nlog;
+
+        [Params(1000, 10000)]
+        public int _inputArgument;
+
+        [GlobalSetup]
+        public void Setup()
+        {
+            _log4Net = LogManager.GetLogger(typeof(EmptyLoggers));
+            _nlog = NLog.LogManager.GetCurrentClassLogger();
+        }
+
+        [Benchmark]
+        public void Log4NetEmpty() => _log4Net.DebugFormat("test {0}", _inputArgument);
+
+        [Benchmark]
+        public void NLogEmpty() => _nlog.Debug("test {0}", _inputArgument);
+    }
+}
---
+++ Performance.Comparison/Comparison.Runner/Start.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using BenchmarkDotNet.Running;
+
+namespace Comparison.Runner
+{
+    internal static class StartClass
+    {
+        private static void Main(string[] args)
+        {
+            //var summary = BenchmarkRunner.Run<Md5VsSha256>();
+        }
+    }
+}
